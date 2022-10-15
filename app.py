0bbcea61cb1346fcd0e107fe6c692dadea6f0a3e
Ne0shka <neosha.73@gmail.com>
2022-10-15 20:51:05 +0400

    Added registration of app commands

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from pathlib import Path
 
 from discord import Intents
 from discord.ext import commands
+from discord import Object as DiscordObject
 
 from bot.plugins import plugins
-from bot.config import COMMAND_PREFIXES
+from bot.config import COMMAND_PREFIXES, GUILD_ID
 
 
 class Bot(commands.Bot):
@@ -13,7 +14,8 @@ class Bot(commands.Bot):
         super().__init__(command_prefix=COMMAND_PREFIXES, intents=self.setup_intents())
         self.path = Path(os.path.dirname(os.path.abspath(__file__)))
 
-    def setup_intents(self):
+    @staticmethod
+    def setup_intents():
         intents = Intents.default()
         intents.members = True
         intents.message_content = True
@@ -21,7 +23,13 @@ class Bot(commands.Bot):
 
     async def setup_hook(self):
         await self.register_plugins()
+        await self.register_app_commands()
 
     async def register_plugins(self):
         for Plugin in plugins:
             await self.add_cog(Plugin(self))
+
+    async def register_app_commands(self):
+        guild = DiscordObject(id=GUILD_ID)
+        self.tree.copy_global_to(guild=guild)
+        await self.tree.sync(guild=guild)
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from envparse import Env
 env = Env()
 env.read_envfile()
 
+GUILD_ID = env.int("GUILD_ID")
 BOT_TOKEN = env.str("BOT_TOKEN")
 COMMAND_PREFIXES = env.list("COMMAND_PREFIXES")
 WELCOME_CHANNEL_ID = env.int("WELCOME_CHANNEL_ID")
